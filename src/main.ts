c456bad5d7b2b721bb4ac5a5372cee272c3662f2
gunatit369 <adityakalmekh2112@gmail.com>
2024-11-05 19:22:58 +0530

    Added Security Policy

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin : `${process.env.CORS_ORIGIN}`,
+    origin : "https://nearby-frontend-psi.vercel.app",
     credentials: true
 }));
 app.use(bodyParser.json());
