96df1873900572bb2bb072c477ca09bb13fc34f6
gunatit369 <adityakalmekh2112@gmail.com>
2024-11-06 09:02:27 +0530

    Made changes vercel.json main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors"
 import bodyParser from "body-parser";
 import "dotenv/config";
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin : "https://nearby-frontend-psi.vercel.app",
+    origin: "https://nearby-frontend-psi.vercel.app",
     credentials: true
 }));
 app.use(bodyParser.json());
@@ -22,6 +22,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // API Routers
 app.use("/nearBy", AuthRoute);
+
+// Base url
+app.get('/', (req, res) => {
+    res.json({ status: 'Server is running' });
+});
+
+app.get('/api/health', (req: Request, res: Response) => {
+    res.status(200).json({
+        status: 'ok',
+        timestamp: new Date().toISOString()
+    });
+});
 
 // Error handler
 app.use(errorHandler);
@@ -39,12 +51,12 @@ const startServer = async () => {
             `);
         });
     } catch (error) {
-        console.error("Failed to start server:",error);
+        console.error("Failed to start server:", error);
         process.exit(1);
     }
 }
 
-startServer().catch((error) =>{
+startServer().catch((error) => {
     console.error('❌ Error:', error);
     process.exit(1);
 });
