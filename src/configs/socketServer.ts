946b1569b815f8c00fa388d0ff07624135fb4f56
gunatit369 <adityakalmekh2112@gmail.com>
2025-01-09 16:41:11 +0530

    updated socket connection

--- src/configs/socketServer.ts
+++ src/configs/socketServer.ts
@@ -10,15 +10,15 @@ export function createSocketServer(httpServer: HTTPServer) {
         cors: {
             origin: `${process.env.CORS_ORIGIN}` || 'https://nearby-frontend-psi.vercel.app',
             credentials: true,
-            methods: ["GET", "POST"]
+            methods: ["GET", "POST"],
+            allowedHeaders: ['Content-Type', 'Authorization']
         },
         path: '/socket.io/',
         transports: ['polling'],
         allowEIO3: true,
         pingTimeout: 60000,
         pingInterval: 25000,
-        connectTimeout: 45000,
-        maxHttpBufferSize: 1e8
+        connectTimeout: 45000
     });
 
     function setupSocketConnections() {
