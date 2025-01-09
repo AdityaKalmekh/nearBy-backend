ad4c79f970f0db38607f9d77a166d380525699a2
gunatit369 <adityakalmekh2112@gmail.com>
2025-01-09 22:39:21 +0530

    Updated env config and package.json

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -2,11 +2,12 @@ import dotenv from 'dotenv';
 
 // Load environment variables once
 const loadEnv = () => {
-    dotenv.config({
-        path: process.env.NODE_ENV === 'production' 
-            ? '.env.production' 
-            : '.env.development'
-    });
+    if (process.env.NODE_ENV !== 'production'){
+         // Only try to load .env file in development
+        dotenv.config({
+            path:'.env.development'
+        });
+    }
     
     console.log('Environment:', process.env.NODE_ENV);
     console.log('Using database:', process.env.MONGODB_URI?.split(':')[0]); // Logs database without credentials
