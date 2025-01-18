046f3ef52a41f613f875e63f241b13a3826b7b4e
gunatit369 <adityakalmekh2112@gmail.com>
2025-01-19 00:43:41 +0530

    Updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -37,6 +37,8 @@ app.use(cors({
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Origin', process.env.CORS_ORIGIN);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Cookie');
     res.header('Access-Control-Expose-Headers', 'Set-Cookie');
     next();
 });
