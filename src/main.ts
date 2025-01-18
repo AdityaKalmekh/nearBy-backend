440d4fb6f1ad5d7c348193efbd2f250c1cd562b6
gunatit369 <adityakalmekh2112@gmail.com>
2025-01-18 18:53:26 +0530

    Updated header in main.ts

--- src/main.ts
+++ src/main.ts
@@ -36,7 +36,7 @@ app.use(cors({
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Origin', `${process.env.CORS_ORIGIN}`);
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS,PATCH');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Cookie');
     next();
 });
