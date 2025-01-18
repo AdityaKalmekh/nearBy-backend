fe06cca451084d185ca0234595183019ed73c053
gunatit369 <adityakalmekh2112@gmail.com>
2025-01-18 21:16:59 +0530

    Updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -33,14 +33,6 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
 }));
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Origin', `${process.env.CORS_ORIGIN}`);
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS,PATCH');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Cookie');
-    next();
-});
-
 //Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
