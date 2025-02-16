5ec7582b4d9eeee902953429a1ec8bbc22f4bdc9
gunatit369 <adityakalmekh2112@gmail.com>
2025-02-16 15:10:16 +0530

    Added middleware for provider location tracking.

--- src/routes/providerLocation.routes.ts
+++ src/routes/providerLocation.routes.ts
@@ -1,9 +1,24 @@
 import express from "express";
 import { providerLocationController } from "../controllers/providerlocation.controller";
+import { asyncHandler } from "../utils/asyncHandler";
+import { jwtService } from "../utils/jwt.utils";
+import { ROLES } from "../configs/roles";
 
 const routes = express.Router();
 
-routes.post("/provider/location/start/:providerId", providerLocationController.startTracking);
-routes.patch("/provider/location/update/:providerId", providerLocationController.updateLocation);
-routes.post("/provider/location/stop/:providerId", providerLocationController.stopTracking);
+routes.post("/provider/location/start/:providerId",
+    asyncHandler(jwtService.authMiddleware),
+    asyncHandler(jwtService.authorize([ROLES.PROVIDER])),
+    providerLocationController.startTracking);
+
+routes.patch("/provider/location/update/:providerId",
+    asyncHandler(jwtService.authMiddleware),
+    asyncHandler(jwtService.authorize([ROLES.PROVIDER])),
+    providerLocationController.updateLocation);
+
+routes.post("/provider/location/stop/:providerId", 
+    asyncHandler(jwtService.authMiddleware),
+    asyncHandler(jwtService.authorize([ROLES.PROVIDER])),
+    providerLocationController.stopTracking);
+    
 export default routes;
